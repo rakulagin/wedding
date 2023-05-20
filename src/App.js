d0d50f3bd86b82b3de572e9acfb635868b84fe0f
rakulagin <koulagin@gmail.com>
2023-05-20 20:02:27 +0400

    добавил картинку

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import PhoneForm from "./components/PhoneForm/PhoneForm";
+import flowers from './img/flowers.png'
 
 function App() {
   return (
     <div className="App">
-      <div className="App-header">
+      <div className='header'></div>
+      <div className="content">
+        <div>
+          <img className='img' src={flowers} alt="flowers"/>
+        </div>
         свадьба
         <form action="#">
+          <PhoneForm/>
           <button>Отправить</button>
         </form>
       </div>
