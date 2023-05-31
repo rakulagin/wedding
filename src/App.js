fd38bfc344b1e8adef81221d2d463f5478842b3e
rakulagin <koulagin@gmail.com>
2023-05-31 20:50:12 +0400

    button

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <img className='img' src={flowers} alt="flowers"/>
       <div className='container'>
-        <h1 className='app-header'>Привет!</h1>
+        <h1 className='app-header'>Привет!!</h1>
         <p className='app-text'>С&nbsp;тобой говорит Рома и&nbsp;Алена. Нам надо узнать, с&nbsp;кем имеем дело. Для этого введи свое имя и&nbsp;фамилию полностью.</p>
         <InputForm/>
       </div>
