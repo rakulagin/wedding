7f6c6518ccf850aa0dda1deebaaefeac7862ad46
rakulagin <koulagin@gmail.com>
2023-06-20 15:53:30 +0400

    теперь картинка не бекграундом, а img

--- src/pages/InvitePage/InvitePage.jsx
+++ src/pages/InvitePage/InvitePage.jsx
@@ -75,8 +75,9 @@ const InvitePage = () => {
     <>
       <div
         className='image img__wrp'
-        style={{backgroundImage: "url('http://backend.rakulagin.com" + userInfo.img + "')"}}
+        // style={{backgroundImage: "url('http://backend.rakulagin.com" + userInfo.img + "')"}}
       >
+        <img className='img' src={`http://backend.rakulagin.com${userInfo.img}`} alt="наше фото"/>
         {userInfo.answered && userInfo.accept === "yes"  && <button onClick={changeInterview} className="btn btn--on-image btn--purple">пройти опрос снова</button>}
         {userInfo.accept === "no" && <button className="btn btn--on-image btn--purple" onClick={changeAccept}>я пойду!</button>}
         <button className='btn--disabled' onClick={clear}>CLEAR</button>
