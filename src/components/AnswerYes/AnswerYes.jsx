df7ce9d268d04fe2bf66d7302d1662158bb8dc42
rakulagin <koulagin@gmail.com>
2023-06-20 13:51:27 +0400

    сделал акцент на приглашении

--- src/components/AnswerYes/AnswerYes.jsx
+++ src/components/AnswerYes/AnswerYes.jsx
@@ -13,7 +13,7 @@ const AnswerYes = () => {
       {/*<h2 className='title'>Отлично, {userInfo.firstName}!</h2>*/}
       <h2 className='title'>{userInfo.nickname}</h2>
       <p className='text'>Мы рады что ты согласился разделить с нами день рождения нашей семьи! </p>
-      <p className='text'>Ждем тебя {userInfo.pair} <br/> к {time} по адресу {address}.</p>
+      <p className='text text--accent'>Ждем тебя {userInfo.pair} <br/> к {time} по адресу {address}.</p>
       <p className='text'>Мы постараемся выбрать для тебя {userInfo.vine} и {userInfo.spirit}. Это будет самая крутая вечеринка!</p>
       {userInfo.secondDay && <p className='text'>{userInfo.secondDay}</p>}
       <p className='text'>Изменить свои предпочтения ты сможешь, нажав кнопку.</p>
